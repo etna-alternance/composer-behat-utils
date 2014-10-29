0202217a245ed0e866d0391f471a043f64d5be02
Stephane CRIVISIER <stephane@crivisier.fr>
2014-10-29 20:07:05 +0100

    Fix des alertes sur ORMProfiler

--- src/ORMProfiler.php
+++ src/ORMProfiler.php
@@ -4,17 +4,19 @@ namespace ETNA\FeatureContext;
 
 trait ORMProfiler
 {
-    static private $currentQuery =  0;
-    static private $max_queries  = 10;
+    static private $query_count = 0;
+    static private $max_queries = 10;
 
     /**
      * @BeforeScenario
      */
-    public function resetProfiler()
+    public function resetProfiler($event)
     {
-        self::$currentQuery += self::$silex_app["orm.profiler"]->currentQuery;
+        self::$query_count += self::$silex_app["orm.profiler"]->currentQuery;
+
         self::$silex_app["orm.profiler"]->queries      = [];
         self::$silex_app["orm.profiler"]->currentQuery = 0;
+
         self::$max_queries = 10;
         if (isset(self::$_parameters['maxQueries']) && self::$_parameters['maxQueries']) {
             self::$max_queries = self::$_parameters['maxQueries'];
@@ -26,7 +28,7 @@ trait ORMProfiler
      */
     public static function showQueryCount()
     {
-        echo "\n# total queries : ", self::$currentQuery, "\n";
+        echo "\n# total queries : ", self::$query_count, "\n";
     }
 
     /**
