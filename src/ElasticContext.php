f66147214cb7732f7754030ea464749c63ab9d92
Jean BILLAUD <billau_j@etna-alternance.net>
2019-03-28 11:08:14 +0100

    [FEATURE#133499] Update des contexts elastic et doctrine

--- src/ElasticContext.php
+++ src/ElasticContext.php
@@ -1,17 +1,30 @@
 <?php
-
 namespace ETNA\FeatureContext;
 
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
+
 
 class ElasticContext extends BaseContext
 {
+    private static $indexed = false;
+
     /**
-     * @BeforeSuite
+     * @BeforeScenario
      */
-    public static function indexElasticSearch()
+    public function indexElasticSearch()
     {
-        passthru("php bin/console elasticsearch:index --reset");
+        if (false === self::$indexed) {
+            $application = new Application($this->getKernel());
+            $application->setAutoExit(false);
+            $application->run(
+                new ArrayInput(['command' => 'elasticsearch:index', '--reset' => true]),
+                new NullOutput()
+            );
+            self::$indexed = true;
+        }
     }
 
     /**
